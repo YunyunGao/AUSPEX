71277621949d997049ce904fdc649f6e0a092b99
YunyunGao <dd.89815@gmail.com>
2024-07-23 23:04:19 +0200

    version number update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools.command import install
 
 setup(
     name='auspex',
-    version='2.3.0',
+    version='2.3.1',
     author='Yunyun Gao',
     author_email='yunyun.gao@desy.de',
     packages=find_packages(),
