9c61c8391a5b19008c7f7102b93b238bc1307705
YunyunGao <dd.89815@gmail.com>
2024-12-04 16:41:08 +0100

    implementing functionality for scaling outlier

--- auspex/lib/Utilities.py
+++ auspex/lib/Utilities.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.linalg import svd
+
 def construct_hl_table(obs):
     obs_ext = obs[None, :] * np.ones(obs.size)[:, None]
     # inv_res_sqr_ext = inv_res_sqr[None, :] * np.ones(inv_res_sqr.size)[:, None]
@@ -7,12 +8,14 @@ def construct_hl_table(obs):
     # inv_res_sqr_ih_table = delete_diag(inv_res_sqr_ext)
     return obs_hl_table
 
+
 def delete_diag(square_matrix):
     # inspired by https://stackoverflow.com/questions/46736258/deleting-diagonal-elements-of-a-numpy-array
     # delete diagonal elements
     m = square_matrix.shape[0]
     s0, s1 = square_matrix.strides
     return np.lib.stride_tricks.as_strided(square_matrix.ravel()[1:], shape=(m-1, m), strides=(s0+s1, s1)).reshape(m, -1)
+
 
 def cal_scaling_outlier(A, i_hl, sig_hl, SdRej=6.):
     U, s, Vh = svd(A)
