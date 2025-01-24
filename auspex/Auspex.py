a931c7f16673cff788807e99b413c210d3cc2d4f
YunyunGao <dd.89815@gmail.com>
2025-01-24 12:22:35 +0100

    add coding format header

--- auspex/Auspex.py
+++ auspex/Auspex.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from ReflectionData import Mtz, Dials, Cif, Xds
 
 from BinnedData import BinnedSummaries
--- auspex/__init__.py
+++ auspex/__init__.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import sys
 import os
 sys.path.append(os.path.dirname(__file__))
